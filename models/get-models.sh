e12d27dec069d535303e5dda2885eec6952c2eef
Billy Stevens <contact@wasv.me>
2021-01-17 14:13:22 -0500

    Updated get-models.sh

--- models/get-models.sh
+++ models/get-models.sh
@@ -9,15 +9,12 @@ if ! $(which gh &>/dev/null); then
     exit 2
 fi
 
-release=$(gh api graphql -F owner=mozilla -F repo=deepspeech -f query="$(cat latest-release.graphql)" | jq -r .data.repository.releases.nodes[].tagName)
->&2 echo "Latest release is: $release"
-
-model=$(gh api graphql --paginate -F owner=mozilla -F repo=deepspeech -F release="$release" -f query="$(cat release-assets.graphql)" \
-    | jq -r '.data.repository.release.releaseAssets.nodes[] | select(.name | endswith("models.pbmm")).downloadUrl')
+model=$(gh api graphql --paginate -F owner=mozilla -F repo=deepspeech -f query="$(cat release-assets.graphql)" \
+    | jq -r '.data.repository.latestRelease.releaseAssets.nodes[] | select(.name | endswith("models.pbmm")).downloadUrl')
 >&2 echo "Link to model is: $model"
 curl -L $model -o deepspeech.pbmm
 
-scorer=$(gh api graphql --paginate -F owner=mozilla -F repo=deepspeech -F release="$release" -f query="$(cat release-assets.graphql)" \
-    | jq -r '.data.repository.release.releaseAssets.nodes[] | select(.name | endswith("models.scorer")).downloadUrl')
+scorer=$(gh api graphql --paginate -F owner=mozilla -F repo=deepspeech -f query="$(cat release-assets.graphql)" \
+    | jq -r '.data.repository.latestRelease.releaseAssets.nodes[] | select(.name | endswith("models.scorer")).downloadUrl')
 >&2 echo "Link to scorer is: $scorer"
 curl -L $scorer -o deepspeech.scorer
